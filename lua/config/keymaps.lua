67ea027ecb0718f08baecf6ec9456768387573f7
Shivaji Chalise <respond2shivaji@gmail.com>
2023-02-23 23:10:37 +0545

    added new plugins and fixed some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<TAB>", vim.cmd.bnext)
-vim.keymap.set("n", "<S-TAB>", vim.cmd.bnext)
+vim.keymap.set("n", "<S-TAB>", vim.cmd.bprev)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
